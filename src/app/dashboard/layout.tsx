9b970dce7d39f60339a957f92b157b72b0b6c75a
Hanieh Eshrati <heshrti200416@gmail.com>
2026-02-07 17:15:11 +0330

    Update layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -43,7 +43,7 @@ export default function DashboardLayout({
           <X size={24} />
         </button>
         <div className="pt-16">
-          <Sidebar />
+          <Sidebar isMobile={true} />
         </div>
       </div>
 
